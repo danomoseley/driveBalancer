b365438a6967ba347be5ece2ac4d7106847ecfa6
Dan Moseley <danomoseley@gmail.com>
2013-06-06 00:30:23 -0400

    New getSize function

--- balance.py
+++ balance.py
@@ -37,24 +37,43 @@ def humanize_bytes(bytes, precision=1):
             break
     return '%.*f %s' % (precision, bytes / factor, suffix)
 
-def getSize(source):
-        total_size = os.path.getsize(source)
-        for item in os.listdir(source):
-            itempath = os.path.join(source, item)
-            if os.path.isfile(itempath):
-                total_size += os.path.getsize(itempath)
-            elif os.path.isdir(itempath):
-                total_size += getSize(itempath)
-        return total_size
+def dirSize(start, follow_links=0, start_depth=0, max_depth=0, skip_errs=0):
+
+    # Get a list of all names of files and subdirectories in directory start
+    try: dir_list = os.listdir(start)
+    except:
+        # If start is a directory, we probably have permission problems
+        if os.path.isdir(start):
+            raise DirSizeError('Cannot list directory %s'%start)
+        else:  # otherwise, just re-raise the error so that it propagates
+            raise
+
+    total = 0L
+    for item in dir_list:
+        # Get statistics on each item--file and subdirectory--of start
+        path = join(start, item)
+        try: stats = os.stat(path)
+        except: 
+            if not skip_errs:
+                raise DirSizeError('Cannot stat %s'%path)
+        # The size in bytes is in the seventh item of the stats tuple, so:
+        total += stats[6]
+        # recursive descent if warranted
+        if isdir(path) and (follow_links or not islink(path)):
+            bytes = dir_size(path, follow_links, start_depth+1, max_depth)
+            total += bytes
+            if max_depth and (start_depth < max_depth):
+                print_path(path, bytes)
+    return total
 
 def getImmediateSubdirectories(dir):
-    return [dir + name for name in os.listdir(dir)
+    return [dir + '\\' + name for name in os.listdir(dir)
             if os.path.isdir(os.path.join(dir, name))]
 
 greatest_free_space = 0
 path_with_most_free_space = ''
 
-paths = ['/Users/Dan-o/Downloads/']
+paths = ['E:\TV Shows', 'G:\TV Shows', 'H:\TV Shows']
 
 for path in paths:
     free_space = get_free_space(path)
@@ -70,7 +89,7 @@ max_size = 0
 for disk in paths:
     if disk not in path_with_most_free_space:
         for dir in getImmediateSubdirectories(disk):
-            dir_size = getSize(dir)
+            dir_size = dirSize(dir)
             if dir_size > max_size and dir_size < (greatest_free_space / 2):
                 max_size = dir_size
                 best_match_folder = dir
@@ -79,7 +98,7 @@ if best_match_folder != '' and path_with_most_free_space != '':
     print best_match_folder
     print humanize_bytes(max_size, 2)
     print
-    shutil.move(best_match_folder, path_with_most_free_space + os.path.basename(best_match_folder))
+    #shutil.move(best_match_folder, path_with_most_free_space + os.path.basename(best_match_folder))
 else:
     print 'error'
 
